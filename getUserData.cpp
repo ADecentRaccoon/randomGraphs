a8084be219c6580a072c7ea669460058c93dcc93
ADecentRaccoon <vovamanuev@gmail.com>
2025-05-07 10:19:06 +0300

    added threshold numbers while getting users data

--- getUserData.cpp
+++ getUserData.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 using namespace std;
 
 struct UsersData{
@@ -14,12 +15,19 @@ UsersData getUserRequest() {
     cout << "G(n,p)[1]" << '\t' << "G(n,m)[2]" << '\t' << "Demo[3]\n";
     cout << "Ender number of model: ";
     cin >> datauser.typeOfModel;
-    if (datauser.typeOfModel == 1){
-        cout << "Enter probability (0 <= P <= 1): ";
+    if (datauser.typeOfModel == 1)
+    {
+        cout << "Enter quantity of nodes: ";
+        cin >> datauser.nodes;
+
+        cout << "Enter probability (0 <= P <= 1): \t" << "connectivity threshold: " <<log(datauser.nodes)/datauser.nodes << endl;
         cin >> datauser.PorVec;
     }else if (datauser.typeOfModel == 2)
     {
-        cout << "Enter number of edges (m): ";
+        cout << "Enter quantity of nodes: ";
+        cin >> datauser.nodes;
+
+        cout << "Enter number of edges (m): \t" << "connectivity threshold: " << datauser.nodes * log(datauser.nodes)/2 << endl;
         cin >> datauser.PorVec;
     }else if (datauser.typeOfModel == 3){
         return datauser;
@@ -27,7 +35,5 @@ UsersData getUserRequest() {
         cout << "Wrong number.\n";
         exit(0);
     }
-    cout << "Enter quantity of nodes: ";
-    cin >> datauser.nodes;
     return datauser;
 }
